60638123a0991e50cce996042d163a46884312f3
Don Hardman <don.hardman@muvon.io>
2021-07-11 19:54:53 +0700

    Fix negative value issue and add test for it

--- src/functions.php
+++ src/functions.php
@@ -12,15 +12,23 @@ namespace Muvon\KISS;
  *   String value of float representation with fraction applied
  */
 function money_v2a(string $value, int $fraction, bool $trim_trailing = false): string {
+  $is_negative = $value[0] === '-';
+  if ($is_negative) {
+    $value = substr($value, 1);
+  }
   $l = strlen($value);
   $a = $l > $fraction ? $value : str_repeat('0', $fraction - $l + 1) . $value;
   $a = substr_replace($a, '.', -$fraction, 0);
 
-  return $trim_trailing ? rtrim($a, '.0') : $a;
+  return ($is_negative ? '-' : '') . ($trim_trailing ? rtrim($a, '.0') : $a);
 }
 
 // Same function to fast up another way conversion
 function money_a2v(string $amount, int $fraction): string {
+  $is_negative = $amount[0] === '-';
+  if ($is_negative) {
+    $amount = substr($amount, 1);
+  }
   if (str_contains($amount, '.')) {
     $n = strtok($amount, '.');
     $f = substr(strtok('.'), 0, $fraction);
@@ -31,6 +39,5 @@ function money_a2v(string $amount, int $fraction): string {
 
   $l = strlen($f);
   $v = $n . $f . ($fraction > $l ? str_repeat('0', $fraction - $l) : '');
-
-  return ltrim($v, '0') ?: '0';
+  return ($is_negative ? '-' : '') . (ltrim($v, '0') ?: '0');
 }
